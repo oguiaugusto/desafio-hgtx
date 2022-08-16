46756b2864b4e58bbb8895f9580f2986b0f36b88
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-08-16 13:09:57 -0300

    chore: create messages enum

---
+++ back-end/src/helpers/Messages.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-shadow
+enum Messages {
+  INTERNAL_SERVER_ERROR = 'Internal Server Error',
+  USER_ALREADY_EXISTS = 'User already exists',
+}
+
+export default Messages;
