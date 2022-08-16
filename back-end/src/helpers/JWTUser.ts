9e15e16d87cc049281115cd9f5d1f83dd9d9d13d
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-08-16 13:09:42 -0300

    chore: create user jwt class
    with methods generate and verify

---
+++ back-end/src/helpers/JWTUser.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import * as jwt from 'jsonwebtoken';
+import { IUserDecoded, IUserToken } from '../modules/User/interfaces/User';
+
+class JWTUser {
+  constructor(
+    private secret: string,
+    private options: jwt.SignOptions,
+  ) {
+    this.secret = secret;
+    this.options = options;
+  }
+
+  public generate(data: IUserToken) {
+    return jwt.sign(data, this.secret, this.options);
+  } 
+
+  public verify(token: string) {
+    return jwt.verify(token, this.secret) as IUserDecoded;
+  }
+}
+
+const { JWT_SECRET = 'secret_key' } = process.env;
+const jwtUser = new JWTUser(JWT_SECRET, { expiresIn: '7d' });
+
+export { JWTUser };
+export default jwtUser;
