84493fa130f22d5956d421b2e77a45331aba5d22
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-08-16 13:11:21 -0300

    chore: add jwt helper user interfaces

--- back-end/src/modules/User/interfaces/User.ts
+++ back-end/src/modules/User/interfaces/User.ts
@@ -1,3 +1,5 @@
+import { JwtPayload } from 'jsonwebtoken';
+
 export interface IUserDTO {
   name: string;
   email: string;
@@ -8,4 +10,19 @@ export interface IUser extends IUserDTO {
   id: number;
   status: string;
   lastUpdate: Date;
+}
+
+export interface IUserToken {
+  id: number;
+  name: string;
+  email: string;
+  status: string;
+}
+
+export interface IUserDecoded extends JwtPayload {
+  user: IUserToken;
+}
+
+export interface IUserPublic extends IUserToken {
+  token: string;
 }
