a28a21a92597a3c7610dc85b5b16977d4f189378
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-08-17 11:23:50 -0300

    feat: style users table header

---
+++ front-end/src/components/styled/StyledUsersTableHeader.ts
@@ -0,0 +1,30 @@
+import { Box, colors } from '@mui/material';
+import { styled } from '@mui/system';
+
+const StyledUsersTableHeader = styled(Box)({
+  display: 'grid',
+  gridTemplateColumns: '20% 66% 14%',
+  alignItems: 'center',
+  width: '60%',
+  '& .new-user-button': {
+    justifySelf: 'center',
+    paddingY: '14px',
+  },
+  '& .search-input': {
+    backgroundColor: colors.grey[300],
+    borderColor: colors.grey[400],
+  },
+  '& .refresh-button': {
+    justifySelf: 'center',
+    backgroundColor: colors.grey[200],
+    color: colors.grey[800],
+    border: `1px solid ${colors.grey[400]}`,
+    boxShadow: '1px 1px 1px 1px rgba(0, 0, 0, 0.1)',
+    ':hover': {
+      backgroundColor: colors.grey[300],
+      color: colors.grey[900],
+    },
+  },
+});
+
+export default StyledUsersTableHeader;
