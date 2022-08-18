ebb232c0b4386d348de6c5d3decfa0dda3a91d8e
Guilherme Augusto <guilhaugusto05@gmail.com>
2022-08-18 11:51:31 -0300

    chore: update database config

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -2,15 +2,15 @@ require('dotenv/config');
 
 const config = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '12345678',
+  password: process.env.DB_PASS || '123456',
   database: process.env.DB_NAME || 'HGTX_DB',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
+  port: Number(process.env.DB_PORT) || 3002,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
   },
   logging: false,
-}
+};
 
 module.exports = config;
